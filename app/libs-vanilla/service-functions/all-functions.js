30f39fe9f1c9a347d028e459002fc63d9ee75075
Slabnevitch <handofschpilrain@mail.ru>
2024-05-24 18:45:15 +0500

    добавил новые сервис ф-ции. в main.min.js

--- app/libs-vanilla/service-functions/all-functions.js
+++ app/libs-vanilla/service-functions/all-functions.js
@@ -139,7 +139,7 @@ function fadeIn(el, timeout = 10, display) {
   setTimeout(() => {
     el.style.opacity = 1;
   }, 10);
-};
+}
 
 function fadeOut(el, timeout){
   el.style.opacity = 1;
@@ -149,7 +149,83 @@ function fadeOut(el, timeout){
   setTimeout(() => {
     el.style.display = 'none';
   }, timeout);
-};
+}
+
+function fadeToggle(el, timeout, display){
+  console.log('fadeToggle');
+  console.log(el);
+  if(el.style.display !== 'none'){
+    fadeOut(el, timeout);
+  }else{
+    fadeIn(el, timeout, display);
+  }
+}
+
+let slideUp = (target, duration=500) => {
+  target.style.transitionProperty = 'height, margin, padding';
+  target.style.transitionDuration = duration + 'ms';
+  target.style.boxSizing = 'border-box';
+  target.style.height = target.offsetHeight + 'px';
+  target.offsetHeight;
+  target.style.overflow = 'hidden';
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  window.setTimeout( () => {
+    target.style.display = 'none';
+    target.style.removeProperty('height');
+    target.style.removeProperty('padding-top');
+    target.style.removeProperty('padding-bottom');
+    target.style.removeProperty('margin-top');
+    target.style.removeProperty('margin-bottom');
+    target.style.removeProperty('overflow');
+    target.style.removeProperty('transition-duration');
+    target.style.removeProperty('transition-property');
+      //alert("!");
+  }, duration);
+}
+
+let slideDown = (target, duration=500) => {
+  target.style.removeProperty('display');
+  let display = window.getComputedStyle(target).display;
+
+  if (display === 'none')
+    display = 'block';
+
+  target.style.display = display;
+  let height = target.offsetHeight;
+  target.style.overflow = 'hidden';
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  target.offsetHeight;
+  target.style.boxSizing = 'border-box';
+  target.style.transitionProperty = "height, margin, padding";
+  target.style.transitionDuration = duration + 'ms';
+  target.style.height = height + 'px';
+  target.style.removeProperty('padding-top');
+  target.style.removeProperty('padding-bottom');
+  target.style.removeProperty('margin-top');
+  target.style.removeProperty('margin-bottom');
+  window.setTimeout( () => {
+    target.style.removeProperty('height');
+    target.style.removeProperty('overflow');
+    target.style.removeProperty('transition-duration');
+    target.style.removeProperty('transition-property');
+  }, duration);
+}
+let slideToggle = (target, duration = 500) => {
+  console.log('slideToggle')
+  if (window.getComputedStyle(target).display === 'none') {
+    return slideDown(target, duration);
+  } else {
+    return slideUp(target, duration);
+  }
+}
 
 function ScrollToSects(opts){
   var _self = this,
@@ -266,6 +342,10 @@ module.exports.addClass = addClass;
 module.exports.isMobile = isMobile;
 module.exports.fadeIn = fadeIn;
 module.exports.fadeOut = fadeOut;
+module.exports.fadeToggle = fadeToggle;
+module.exports.slideUp = slideUp;
+module.exports.slideDown = slideDown;
+module.exports.slideToggle= slideToggle;
 module.exports.ScrollToSects = ScrollToSects;
 module.exports.multiDropdown = multiDropdown;
 module.exports.singleDropdown = singleDropdown;
